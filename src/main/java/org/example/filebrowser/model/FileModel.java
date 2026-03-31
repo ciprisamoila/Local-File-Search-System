bcd0dc2c9aa584b6092ca1aa6becbb629bb0b468
Samoila Ciprian <ciprisamoila@gmail.com>
2026-03-31 20:02:53 +0300

    feat(model): add file model to pass to db
    FileModel - contains data (file attributes, checksum, content, last scan id) to be passed to the database

---
+++ src/main/java/org/example/filebrowser/model/FileModel.java
@@ -0,0 +1,9 @@
+package org.example.filebrowser.model;
+
+public record FileModel(
+        FileAttributes fileAttributes,
+        long checksumValue,
+        String[] content,
+        long lastScanId
+) {
+}
