08d84dd474f0ab3d3aceeac9884be6890dc7e407
Samoila Ciprian <ciprisamoila@gmail.com>
2026-04-27 10:48:52 +0300

    feat(model): update QueryFileModel to pass all useful data

--- src/main/java/org/example/filebrowser/model/QueryFileModel.java
+++ src/main/java/org/example/filebrowser/model/QueryFileModel.java
@@ -3,7 +3,11 @@ package org.example.filebrowser.model;
 public record QueryFileModel(
         String fullName, // name and extension
         String path,
-        String headline,
-        boolean readAccess
+        String creation_time,
+        String last_modified_time,
+        String last_accessed_time,
+        long size,
+        boolean readAccess,
+        String headline
 ) {
 }
